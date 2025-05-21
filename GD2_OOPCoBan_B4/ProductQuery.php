c432004cf318782bea749cbfd30478fb48ba17db
thuyvt <thuyvt1005@gmail.com>
2025-05-21 13:57:20 +0700

    buổi 5

--- GD2_OOPCoBan_B4/ProductQuery.php
+++ GD2_OOPCoBan_B4/ProductQuery.php
@@ -1,4 +1,5 @@
 <?php 
+    include "Product.php";
     class ProductQuery {
 
         // Khai báo thuộc tính
@@ -18,6 +19,97 @@
         public function __destruct() {
             // đóng kết nối với csdl
             $this->pdo = null;
+        }
+
+        // Hàm lấy danh sách
+        public function all() {
+            try {
+                // 1. Khai báo câu lện 
+                $sql = "SELECT * FROM product";
+
+                // 2. Thực hiện truy vấn và lấy giá trị trả 
+                $data = $this->pdo->query($sql)->fetchAll();
+                // var_dump($data);
+
+                // 3. Convert data sang object Product
+                $danhSachSP = [];
+                foreach ($data as $value) {
+                    $product = new Product();
+                    $product->id = $value["id"];
+                    $product->name = $value["name"];
+                    $product->price  = $value["price"];
+                    $product->quantity = $value["quantity"];
+
+                    // thêm object danhsachSP
+                    $danhSachSP[] = $product;
+                }
+                return $danhSachSP;
+                
+            } catch(Exception $error) {
+                echo "Lỗi" .$error->getMessage() . "<br>";
+            }
+        }
+
+        // Hàm lấy thông tin chi tiết bằng id
+        public function find($id) {
+            try {
+                // 1. Khai báo sql lấy chi tiết
+                $sql = "SELECT * FROM `product` WHERE id = $id";
+
+                // 2. Thực thi truy vấn và lấy giá trị
+                $data = $this->pdo->query($sql)->fetch();
+
+                // 3. Convert dữ liệu sang object Product
+                // kiểm tra xem bản ghii có tồn tại hay khôn
+                // Nếu không tồn tại $data == false
+                if ($data !== false) {
+                    $product = new Product();
+                    $product->id = $data["id"];
+                    $product->name = $data["name"];
+                    $product->price = $data["price"];
+                    $product->quantity = $data["quantity"];
+                    return $product;
+                } else {
+                    echo "// Bản ghi không tồn tại. Vui lòng thử lại <br>";
+                    return;
+                }
+            } catch(Exception $error) {
+                echo "Lỗi: " .$error->getMessage(). "<br>";
+
+            }
+        }
+
+        // Hàm xóa bản ghi trong csdl
+        public function delete($id) {
+            try {
+                // 1. Câu lệnh sql
+                $sql =  "DELETE FROM product WHERE `product`.`id` = $id";
+
+                // 2. thực thi 
+                $data = $this->pdo->exec($sql);
+                // Lưu ý: $data = 1 nếu thành công
+                // Thất bại thì nảy xuống Exception
+                return $data;
+
+            } catch(Exception $error) {
+                echo "Lỗi: ". $error->getMessage(). "<br>";
+
+            }
+        }
+
+        // Hàm xử lý thêm mới sản phẩm
+        public function insert(Product $product) {
+            try {
+
+                $sql = "INSERT INTO `product` (`id`, `name`, `price`, `quantity`)
+                 VALUES (NULL, '".$product->name."', '".$product->price."', '".$product->quantity."');";
+                $data = $this->pdo->exec($sql);
+                // Lưu ý: $data= 1 nếu thành công
+                return $data;
+
+            }catch (Exception $error) {
+                echo "Lỗi: " . $error->getMessage();
+            }
         }
     }
 ?>
--- GD2_OOPCoBan_B4/su_dung_class.php
+++ GD2_OOPCoBan_B4/su_dung_class.php
@@ -4,5 +4,42 @@
     // Tạo object từ ProductQuery mở kết nối tới CSDL
     $productQuery = new ProductQuery();
     
+    // Gọi hàm lấy danh sách
+    echo "Danh sách sản phẩm: <br>";
+    $danhSach = $productQuery->all();
+    if(count($danhSach) > 0) {
+        var_dump($danhSach);
+    } else {
+        echo "Danh sách sản phẩm trống";
+    }
+    echo "<hr>";
+
+    // Gọi hàm lấy thông tin chi tiết
+    echo "Chi tiết sản phẩm: <br>";
+    $product01 = $productQuery->find(8);
+    var_dump($product01);
+    echo "<hr>";
+
+    // Gọi hàm xóa 
+    echo "Xóa thông tin sản phẩm: <br>";
+    $data = $productQuery->delete(2);
+    if ($data === 1) {
+        echo "Xóa sản phẩm thành công <hr>";
+    } else {
+        echo "Xóa sản phẩm thất bại <hr>";
+    }
+
+    // Gọi hàm thêm
+    echo "Thêm mới sản phẩm: <br>";
+    $product02 = new Product();
+    $product02->name = "OPPO R7";
+    $product02->price = 43999;
+    $product02->quantity = 150;
+    $data01 = $productQuery->insert($product02);
+    if($data01 === 1) {
+        echo "Thêm thành công <hr>";
+    } else {
+        echo "Thêm thất bại <hr>";
+    }
 
 ?>
